30e8a82cf83998d248e3208582275d244e0e2650
ayushmishra <mail81mitthu@gmail.com>
2022-06-25 21:56:19 +0530

    intial commit

---
+++ CMakeFiles/NoCol.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ayu/Code/random/NoCol/src/display_manager.cpp" "/home/ayu/Code/random/NoCol/CMakeFiles/NoCol.dir/src/display_manager.cpp.o"
+  "/home/ayu/Code/random/NoCol/src/main.cpp" "/home/ayu/Code/random/NoCol/CMakeFiles/NoCol.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "include"
+  "lib"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/NoCol.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NoCol.dir/src/display_manager.cpp.o"
+  "CMakeFiles/NoCol.dir/src/main.cpp.o"
+  "NoCol"
+  "NoCol.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NoCol.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
